7471ae872c3b97e4f80a83b3951c1d1cc9e342a4
alaneuler <alaneuler@qq.com>
2022-10-19 07:54:50 +0800

    Initial Commit

---
+++ macOSBatteryManager/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  macOSBatteryManager
+//
+//  Created by Alaneuler Erving on 2022/10/19.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ macOSBatteryManager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSBatteryManager
+//
+//  Created by Alaneuler Erving on 2022/10/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
