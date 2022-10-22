80a688176de46fd579b560c01f0a76ee328b5f8c
alaneuler <alaneuler@qq.com>
2022-10-22 08:53:43 +0800

    add uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 #  Created by Alaneuler Erving on 2022/10/22.
-#  
+#
+
+sudo launchctl unload /Library/LaunchDaemons/me.alaneuler.mbm.PrivilegeHelper.plist
+sudo rm -rf /Library/LaunchDaemons/me.alaneuler.mbm.PrivilegeHelper.plist
+sudo rm -rf /Library/PrivilegedHelperTools/me.alaneuler.mbm.PrivilegeHelper
